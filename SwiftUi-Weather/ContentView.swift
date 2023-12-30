f00eb077f35d5591f780995a18ecee0149ad32f1
francescopanico <frpanico96@gmail.com>
2023-12-30 23:34:33 +0100

    [SwiiftUI] Weather app - first commit

--- SwiftUi-Weather/ContentView.swift
+++ SwiftUi-Weather/ContentView.swift
@@ -9,16 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                    WeatherDayView(dayOfWeek: "Cupertino, CA",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 76,
+                                   dayOfWeekSize: 32,
+                                   imageFrame: 180,
+                                   temperatureSize: 70)
+                HStack{
+                    WeatherDayView(dayOfWeek: "Tue", 
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 76,
+                                   dayOfWeekSize: 20,
+                                   imageFrame: 40,
+                                   temperatureSize: 30)
+                }
+                Spacer()
+                
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    var dayOfWeekSize: CGFloat
+    var imageFrame: CGFloat
+    var temperatureSize: CGFloat
+    
+    var body: some View {
+        VStack(spacing: 10){
+                Text(dayOfWeek)
+                    .font(.system(size: dayOfWeekSize, weight: .medium))
+                    .foregroundStyle(.linearGradient(colors: [Color.white],
+                                                     startPoint: .topLeading,
+                                                     endPoint: UnitPoint.bottomTrailing))
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: imageFrame, height: imageFrame)
+            
+            Text("76°")
+                .font(.system(size: temperatureSize, weight: .medium))
+                .foregroundStyle(.linearGradient(colors: [Color.white],
+                                                 startPoint: .topLeading,
+                                                 endPoint: UnitPoint.bottomTrailing))
+        }
+    }
 }
